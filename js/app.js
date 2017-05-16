9c056191ec795474d68850f2e932e87a0627bb11
Angie Barrantes <angiefiorellacat@gmail.com>
2017-05-16 17:43:02 -0500

    I've added the index

---
+++ js/app.js
@@ -0,0 +1,54 @@
+var app = angular.module('myApp', []);
+app.controller('myController', function($scope){
+  $scope.students = [
+  {
+    name : 'Angie',
+    lastname : 'Barrantes',
+    age : 22
+  },
+  {
+    name : 'Yuto',
+    lastname : 'Nakajima',
+    age : 23
+  },
+  {
+    name : 'Ryosuke',
+    lastname : 'Yamada',
+    age : 24
+  },
+  {
+    name : 'Kei',
+    lastname : 'Inoo',
+    age : 26
+  },
+  {
+    name : 'Joseph',
+    lastname : 'Insil',
+    age : 24
+  }
+  ];
+  $scope.studentsToAdd = [
+  {
+    name : '',
+    lastname : '',
+    age : 0
+  }];
+  $scope.deleteStudent = function(x){
+    var indexStudent = $scope.students.indexOf(x);
+    $scope.students.splice(indexStudent, 1);
+
+  }
+  $scope.addStudent = function(){
+    document.getElementById("js-form").className="visible";
+  }
+  $scope.submitNewStudent= function(x){
+    var index = $scope.studentsToAdd.indexOf(x);
+    $scope.studentsToAdd.splice(index, 1);
+    $scope.studentsToAdd.push(angular.copy(x));
+    $scope.students.push(x);
+  }
+});
+
+
+
+
